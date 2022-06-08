95d7e0ba6f764db51c25513a49628ef2e14a44e8
karatikidd94 <alexazevedo9494@gmail.com>
2022-06-08 11:21:24 -0700

    checkers-v2 Update

--- js/checkers.js
+++ js/checkers.js
@@ -1,32 +1,37 @@
 
-const board = [null, '0' , null, '1' , null, '2',  null, '3' ,
-               '4' , null, '5' , null, '6' , null, '7' , null,
-               null, '8' , null, '9' , null, '10', null, '11',
+const board = [null, 'A' , null, 'B' , null, 'C',  null, 'D' ,
+               'E' , null, 'F' , null, 'G' , null, 'H' , null,
+               null, 'I' , null, 'J' , null, 'K', null, 'L',
                null, null, null, null, null, null, null, null,
                null, null, null, null, null, null, null, null,
-               '12', null, '13', null, '14', null, '15', null,
-               null, '16', null, '17', null, '18', null, '19',
-               '20', null, '21', null, '22', null, '23', null];
+               'M', null, 'N', null, 'O', null, 'P', null,
+               null, 'Q', null, 'R', null, 'S', null, 'T',
+               'U', null, 'V', null, 'W', null, 'X', null];
 
 const squares = document.querySelectorAll('td');
 let blackCheckers = document.querySelectorAll('p.black-checker');
 let redCheckers = document.querySelectorAll('p.red-checker');
-let id;
-let black = `<p class="black-checker" id="${id}"></p>`;
-let red = `<p class="red-checker" id="${id}"></p>`;
+let idString;
+let black = `<p class="black-checker" id="${idString}"></p>`;
+let red = `<p class="red-checker" id="${idString}"></p>`;
 let turn = true; // true = red, false = black
 let playerChecker;
 
 let selectedChecker = {
     checkerId: -1,
+    checkerIdx: -1,
     indexOfBoardChecker: -1,
     isKing: false,
     seventhSpace: false,
+    spaceSevenIdx: -1,
     ninthSpace: false,
+    spaceNineIdx: -1,
     fourteenthSpace: false,
     eighteenthSpace: false,
     minusSeventhSpace: false,
+    spaceMinusSeven: -1,
     minusNinthSpace: false,
+    spaceMinusNine: -1,
     minusFourteenthSpace: false,
     minusEighteenthSpace: false
 }
@@ -34,121 +39,217 @@ let selectedChecker = {
 
 function giveCheckersEventListeners() {
     if(turn) {
-        for(let i = 0; i < redCheckers.length; i++) {
-            redCheckers[i].addEventListener('click', function(evt) {
+        for(let i = 0; i < squares.length; i++) {
+            squares[i].addEventListener('click', function(evt) {
                 console.log(`Clicked Red: ${evt.target.id}`);
-                playerChecker = evt.target.id;
-                if(document.getElementById(`${playerChecker}`).hasAttribute('style')) {
-                    deselectChecker(playerChecker);
+                playerChecker = evt.target.id;  // Returns: '0-63' , 'A-W'
+                selectedChecker.indexOfBoardChecker = playerChecker;
+
+                if(selectedChecker.spaceSevenIdx == selectedChecker.indexOfBoardChecker || selectedChecker.spaceNineIdx == selectedChecker.indexOfBoardChecker) {
+                    console.log('I moved Red!');
+                    // console.log(selectedChecker);
+                    deselectChecker(playerChecker, selectedChecker.checkerIdx);
+                    moveChecker(selectedChecker.checkerId, selectedChecker.indexOfBoardChecker, playerChecker);
+                    console.log(selectedChecker);
+                    turn = false;
+                    
+                } else if(document.getElementById(`${playerChecker}`).hasAttribute('style') && playerChecker != null) {
+                    console.log('I cleared Red!');
+                    // console.log(selectedChecker);
+                    deselectChecker(playerChecker, selectedChecker.checkerIdx);
+                    console.log(selectedChecker);
+                    
+
                 } else {
+                    console.log('I selected Red!');
                     selectChecker(playerChecker);
-                    getAvailableMoves(playerChecker);
+                    getAvailableMoves(playerChecker, evt);
+                    console.log(selectedChecker);
+                    
+
                 } 
             });
         }
-        //turn = false;
-    } else {
-        for(let i = 0; i < blackCheckers.length; i++) {
-            blackCheckers[i].addEventListener('click', function(evt) {
+
+    } else if(turn == false){
+        for(let i = 0; i < squares.length; i++) {
+            squares[i].addEventListener('click', function(evt) {
                 console.log(`Clicked Black: ${evt.target.id}`);
-                playerChecker = evt.target.id;
-                if(document.getElementById(`${playerChecker}`).hasAttribute('style')) {
-                    deselectChecker(playerChecker);
+                playerChecker = evt.target.id;  // Returns: '0-63' , 'A-W'
+                selectedChecker.indexOfBoardChecker = playerChecker;
+
+                if(selectedChecker.spaceMinusSeven == selectedChecker.indexOfBoardChecker || selectedChecker.spaceMinusNine == selectedChecker.indexOfBoardChecker) {
+                    console.log('I moved Black!');
+                    // console.log(selectedChecker);
+                    deselectChecker(playerChecker, selectedChecker.checkerIdx);
+                    moveChecker(selectedChecker.checkerId, selectedChecker.indexOfBoardChecker, playerChecker);
+                    console.log(selectedChecker);
+                    turn = true;
+                    
+                } else if(document.getElementById(`${playerChecker}`).hasAttribute('style') && playerChecker != null) {
+                    console.log('I cleared Black!');
+                    // console.log(selectedChecker);
+                    deselectChecker(playerChecker, selectedChecker.checkerIdx);
+                    console.log(selectedChecker);
+                    
+
                 } else {
+                    console.log('I selected Black!');
                     selectChecker(playerChecker);
                     getAvailableMoves(playerChecker, evt);
+                    console.log(selectedChecker);
+                    
+
                 } 
             });
         }
     }
 }
 
-function deselectChecker(a) { 
-    console.log(a);
-    let idx = board.indexOf(a);
+function resetBoard() {
+
+}
+
+function deselectChecker(a , b) { 
+    console.log(`CheckerIdx: ${b}`);
     if(turn) {
-        document.getElementById(`${a}`).removeAttribute('style');
-        document.getElementById(`s${idx-7}`).removeAttribute('style');
-        document.getElementById(`s${idx-9}`).removeAttribute('style');
-        document.getElementById(`s${idx-14}`).removeAttribute('style');
-        document.getElementById(`s${idx-18}`).removeAttribute('style');
+        document.getElementById(b).removeAttribute('style');
+        selected = false;
+        document.getElementById(b-7).removeAttribute('style');
+        selectedChecker.seventhSpace = false;
+        document.getElementById(b-9).removeAttribute('style');
+        selectedChecker.ninthSpace = false;
+        document.getElementById(b-14).removeAttribute('style');
+        selectedChecker.fourteenthSpace = false;
+        document.getElementById(b-18).removeAttribute('style');
+        selectedChecker.eighteenthSpace = false;
+        
     } else {
-        document.getElementById(`${a}`).removeAttribute('style');
-        document.getElementById(`s${idx+7}`).removeAttribute('style');
-        document.getElementById(`s${idx+9}`).removeAttribute('style');
-        document.getElementById(`s${idx+14}`).removeAttribute('style');
-        document.getElementById(`s${idx+18}`).removeAttribute('style');
+        document.getElementById(b).removeAttribute('style');
+        selected = false;
+        document.getElementById(b+7).removeAttribute('style');
+        selectedChecker.seventhSpace = false;
+        document.getElementById(b+9).removeAttribute('style');
+        selectedChecker.ninthSpace = false;
+        document.getElementById(b+14).removeAttribute('style');
+        selectedChecker.fourteenthSpace = false;
+        document.getElementById(b+18).removeAttribute('style');
+        selectedChecker.eighteenthSpace = false;
     }
-    
+
 }
 
 function selectChecker(a) {
-    console.log(document.getElementById(`${a}`));
-    document.getElementById(`${a}`).style.border = '5px solid green';
-    // grab id 
+        let idx = board.indexOf(a);
+        selectedChecker.checkerId = a;
+        selectedChecker.spaceSevenIdx = idx-7;
+        selectedChecker.spaceNineIdx = idx-9;
+        selectedChecker.spaceMinusSeven = idx+7
+        selectedChecker.spaceMinusNine = idx+9
+        if(document.getElementById(board[idx]).hasAttribute('class')) {
+            selectedChecker.checkerIdx = idx;
+            console.log(document.getElementById(`${a}`));
+            document.getElementById(`${a}`).style.border = '5px solid green';
+        }
 }
 
 function getAvailableMoves(m, evt) {
     let idx = board.indexOf(m);
-    let spaceSevenR = document.getElementById(`s${idx-7}`)
-    let spaceNineR = document.getElementById(`s${idx-9}`);
-    let spaceFourteenR = document.getElementById(`s${idx-14}`);
-    let spaceEighteenR = document.getElementById(`s${idx-18}`);
-    let spaceSevenB = document.getElementById(`s${idx+7}`)
-    let spaceNineB = document.getElementById(`s${idx+9}`);
-    let spaceFourteenB = document.getElementById(`s${idx+14}`);
-    let spaceEighteenB = document.getElementById(`s${idx+18}`);
-    console.log(`Id #: ${m}`);
-    console.log(`Index: ${idx}`);
-    console.log(`s7 Type: ${typeof(board[idx-7])}`);
+    // selectedChecker.checkerId = m;
+    selectedChecker.indexOfBoardChecker = idx;
+    let spaceSevenR = document.getElementById(idx-7);
+    let spaceNineR = document.getElementById(idx-9);
+    let spaceFourteenR = document.getElementById(idx-14);
+    let spaceEighteenR = document.getElementById(idx-18);
+    let spaceSevenB = document.getElementById(idx+7)
+    let spaceNineB = document.getElementById(idx+9);
+    let spaceFourteenB = document.getElementById(idx+14);
+    let spaceEighteenB = document.getElementById(idx+18);
     
     if(turn) { // Available moves for Red Checkers
-         if(board[idx - 7] == null) {
+         if(board[idx - 7] == null && spaceSevenR.classList.contains('noPiece') == false) {
             console.log(`Plus Seven Spaces Red: ${board[idx - 7]}`);
+            selectedChecker.seventhSpace = true;
             spaceSevenR.style.border = '3px solid red';
             spaceSevenR.style.cursor = 'pointer';
          }
-         if(board[idx - 9] == null ) { 
+         if(board[idx - 9] == null && spaceNineR.classList.contains('noPiece') == false) { 
             console.log(`Plus Nine Spaces Red: ${board[idx - 9]}`);
+            selectedChecker.ninthSpace = true;
             spaceNineR.style.border = '3px solid red';
             spaceNineR.style.cursor = 'pointer';
          } 
-         if(board[idx -7] != null) {
+         if(board[idx - 7] != null && board[idx - 14] == null && spaceFourteenR.classList.contains('noPiece') == false && document.getElementById(board[idx-7]).classList.contains('black-checker') == true) {
             console.log(`Plus Fourteen Spaces Red: ${board[idx - 14]}`);
+            selectedChecker.fourteenthSpace = true;
             spaceFourteenR.style.border = '3px solid red';
             spaceFourteenR.style.cursor = 'pointer';
          }
-         if(board[idx -9] != null) {
+         if(board[idx - 9] != null && board[idx - 18] == null && spaceEighteenR.classList.contains('noPiece') == false && document.getElementById(board[idx-9]).classList.contains('black-checker') == true) {
             console.log(`Plus Eighteen Spaces Red: ${board[idx - 18]}`);
+            selectedChecker.eighteenthSpace = true;
             spaceEighteenR.style.border = '3px solid red';
             spaceEighteenR.style.cursor = 'pointer';
          }
         
             
     } else { // Available moves for Black Checkers
-        if(board[idx + 7] == null) {
-            console.log(`Plus Seven Spaces Red: ${board[idx - 7]}`);
+        if(board[idx + 7] == null && spaceSevenB.classList.contains('noPiece') == false) {
+            console.log(`Plus Seven Spaces Black: ${board[idx + 7]}`);
+            selectedChecker.minusSeventhSpace = true;
             spaceSevenB.style.border = '3px solid red';
             spaceSevenB.style.cursor = 'pointer';
          }
-         if(board[idx + 9] == null) { 
-            console.log(`Plus Nine Spaces Red: ${board[idx - 9]}`);
+         if(board[idx + 9] == null && spaceNineB.classList.contains('noPiece') == false) { 
+            console.log(`Plus Nine Spaces Black: ${board[idx + 9]}`);
+            selectedChecker.minusNinthSpace = true;
             spaceNineB.style.border = '3px solid red';
             spaceNineB.style.cursor = 'pointer';
          } 
-         if(board[idx + 7] != null) {
-            console.log(`Plus Fourteen Spaces Red: ${board[idx - 14]}`);
+         if(board[idx + 7] != null && board[idx + 14] == null && spaceFourteenR.classList.contains('noPiece') == false && document.getElementById(board[idx+7]).classList.contains('red-checker') == true) {
+            console.log(`Plus Fourteen Spaces Black: ${board[idx + 14]}`);
+            selectedChecker.minusFourteenthSpace = true;
             spaceFourteenB.style.border = '3px solid red';
             spaceFourteenB.style.cursor = 'pointer';
          }
-         if(board[idx + 9] != null) {
-            console.log(`Plus Eighteen Spaces Red: ${board[idx - 18]}`);
+         if(board[idx + 9] != null && board[idx + 18] == null && spaceEighteenB.classList.contains('noPiece') == false && document.getElementById(board[idx+9]).classList.contains('red-checker') == true) {
+            console.log(`Plus Eighteen Spaces Black: ${board[idx + 18]}`);
+            selectedChecker.minusEighteenthSpace = true;
             spaceEighteenB.style.border = '3px solid red';
             spaceEighteenB.style.cursor = 'pointer';
          }
     }
     
 }
+
+function moveChecker(id, idx, newidx) {
+    console.log(`Index Move: ${idx}`);
+    let newIdx = selectedChecker.checkerIdx;
+    console.log(newIdx);
+    document.getElementById(newIdx).innerHTML = '';
+    board[newIdx] = null;
+    if(turn) {
+        idString = id;
+        document.getElementById(newidx).innerHTML = `<p class="red-checker" id="${idString}"></p>`;
+        board[newidx] = id;
+        selectedChecker.checkerId = -1;
+        selectedChecker.checkerIdx = -1;
+        selectedChecker.indexOfBoardChecker = -1;
+        selectedChecker.spaceSevenIdx = -1;
+        selectedChecker.spaceNineIdx = -1;
+    } else {
+        idString = id;
+        document.getElementById(newidx).innerHTML = `<p class="black-checker" id="${idString}"></p>`;
+        board[newidx] = id;
+        selectedChecker.checkerId = -1;
+        selectedChecker.checkerIdx = -1;
+        selectedChecker.indexOfBoardChecker = -1;
+        selectedChecker.spaceMinusSeven = -1;
+        selectedChecker.spaceMinusNine = -1;
+    }
+}
+
+
 
 
 giveCheckersEventListeners();
